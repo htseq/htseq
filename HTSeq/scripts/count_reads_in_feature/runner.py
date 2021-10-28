fface87a8f014a31c8d7373feccdf82c888ade51
Givanna Putri <ghar1821@users.noreply.github.com>
2021-10-28 13:46:04 +1100

    renaming

--- HTSeq/scripts/count_reads_in_feature/runner.py
+++ HTSeq/scripts/count_reads_in_feature/runner.py
@@ -2,20 +2,20 @@ import warnings
 import argparse # needed?
 
 import HTSeq
-import HTSeq.scripts.count_reads_in_feature.process_options as process_options
+import HTSeq.scripts.count_reads_in_feature.arguments_processor as ap
 
 
 # Call me if you want to run htseq-count!
 def main():
 
-    pa = process_options.get_help_message()
+    pa = ap.get_help_message()
     args, argv = pa.parse_known_args()
     # Version is the only case where the BAM and GTF files are optional
     if args.version:
         print(HTSeq.__version__)
         sys.exit()
 
-    process_options.add_options()
+    ap.add_options()
 
     args = pa.parse_args()
 
