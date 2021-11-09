4944ceffd057b423dcdc77fb0ebb145971bf52d1
Givanna Putri <ghar1821@users.noreply.github.com>
2021-11-10 00:14:35 +1100

    Update count_features_per_file.py
    add doc and renaming variables

--- HTSeq/scripts/count_features/count_features_per_file.py
+++ HTSeq/scripts/count_features/count_features_per_file.py
@@ -2,9 +2,6 @@ import itertools
 import random
 import sys
 
-import pysam
-
-import HTSeq
 from HTSeq.scripts.utils import invert_strand, UnknownChrom
 from HTSeq.scripts.count_features.reads_io_processor import ReadsIO
 from HTSeq.scripts.count_features.reads_stats import ReadsStatistics
@@ -31,30 +28,67 @@ def count_reads_single_file(
         samout_filename,
 ):
     """
+    The function that does the counting for each input BAM/SAM file.
     Fixme: there are some redundant parameters here.. feature_type, id_attribute, additional_attributes
+
     Parameters
     ----------
-    isam
-    sam_filename
-    features
-    feature_attr
-    order
-    max_buffer_size
-    stranded
-    overlap_mode
-    multimapped_mode
-    secondary_alignment_mode
-    supplementary_alignment_mode
-    feature_type
-    id_attribute
-    additional_attributes
-    quiet
-    minaqual
-    samout_format
-    samout_filename
-
+    isam : int
+        input files' indexing for the purpose of parallel processing.
+        This basically tell you which input file is being processed by this
+        instance of function.
+    sam_filename : str
+        Path to the SAM/BAM file containing the mapped reads.
+    features : array
+        TODO check the type of this parameter.
+        Supplied by HTSeq.make_feature_genomicarrayofsets
+    feature_attr : array
+        TODO check the type of this parameter.
+        Supplied by HTSeq.make_feature_genomicarrayofsets
+    order : str
+        Can only be either 'pos' or 'name'. Sorting order of <alignment_file>.
+    max_buffer_size : int
+        The number of reads allowed to stay in memory until mates are found.
+        Used when <alignment_file> is paired end sorted by position.
+    stranded : str
+        Whether the data to be aligned is from a strand-specific assay.
+        Option is yes, no, reverse.
+        reverse means yes with reversed strand interpretation.
+    overlap_mode : str
+        Mode to handle reads overlapping more than one feature.
+        Choices: union, intersection-strict, intersection-nonempty.
+    multimapped_mode : str
+        Whether and how to score reads that are not uniquely aligned or
+        ambiguously assigned to features.
+        Choices: none, all, fraction, random.
+    secondary_alignment_mode : str
+        Whether to score secondary alignments (0x100 flag).
+        Choices: score or ignore.
+    supplementary_alignment_mode : str
+        Whether to score supplementary alignments (0x800 flag).
+        Choices: score or ignore.
+    feature_type : str
+        Feature type (3rd column in GTF file) to be used, all features of other
+        type are ignored (default, suitable for Ensembl, GTF files: exon).
+    id_attribute : str
+        GTF attribute to be used as feature ID.
+        Normally gene_id, suitable for Ensembl GTF files.
+    additional_attributes : array
+        Additional feature attributes.
+        Commonly, gene_name is suitable for Ensembl GTF files.
+    quiet : boolean
+        Whether to suppress progress report.
+    minaqual : int
+        Value denoting the MAPQ alignment quality of reads to skip.
+    samout_format : str
+        Format of the output files denoted by samouts.
+        Choices: SAM, BAM, sam, bam.
+    samout_filename : str
+        The name of SAM/BAM file to write out all SAM alignment records into.
     Returns
     -------
+    Dictionary
+        TODO update me when done refactoring
 
     """
     try:
@@ -138,14 +172,29 @@ def count_reads_single_file(
     return res
 
 
-def _update_feature_set_counts(fs, multimapped_mode, r, read_stats):
+def _update_feature_set_counts(fs, multimapped_mode, read_sequence, read_stats):
+    """
+    Distribute the counts among the aligned feature set.
+
+    Parameters
+    ----------
+    fs : array
+        A list of feature set previously aligned to the read
+    multimapped_mode : str
+        How to handle read mapped to multiple features
+    read_sequence : array
+        Read sequence
+    read_stats : ReadsStatistics object
+        For updating bad reads
+
+    """
     if fs is None or len(fs) == 0:
-        read_stats.add_empty_read(read_sequence=r)
+        read_stats.add_empty_read(read_sequence=read_sequence)
     elif len(fs) > 1:
-        read_stats.add_ambiguous_read(read_sequence=r,
+        read_stats.add_ambiguous_read(read_sequence=read_sequence,
                                       assignment="__ambiguous[" + '+'.join(sorted(fs)) + "]")
     else:
-        read_stats.add_good_read_assignment(read_sequence=r, assignment=list(fs)[0])
+        read_stats.add_good_read_assignment(read_sequence=read_sequence, assignment=list(fs)[0])
     if fs is not None and len(fs) > 0:
         fs = list(fs)
         if multimapped_mode == 'none':
@@ -166,6 +215,27 @@ def _update_feature_set_counts(fs, multimapped_mode, r, read_stats):
 
 
 def _align_reads_to_feature_set(features, iv_seq, overlap_mode):
+    """
+    Align reads to feature set.
+
+    Parameters
+    ----------
+    features : array
+        A set of features to align the reads to
+        TODO not sure the type yet.
+    iv_seq : array
+        TODO not sure the type yet.
+        Read (or interval?) sequence
+    overlap_mode : str
+        How to select the features for read that not 100% aligned to a feature.
+
+    Returns
+    -------
+    fs : array
+        A set of features to align the reads to
+        TODO not sure the type yet.
+
+    """
     if overlap_mode == "union":
         fs = set()
         for iv in iv_seq:
@@ -192,6 +262,29 @@ def _align_reads_to_feature_set(features, iv_seq, overlap_mode):
 
 
 def _get_iv_seq_pe_read(com, r, stranded):
+    """
+    Function to break down the read sequence into intervals which will
+    subsequently be processed.
+
+    Parameters
+    ----------
+    com : array
+        CIGAR match characters (including alignment match, sequence match, and
+        sequence mismatch
+    r :
+        todo update type
+        Read sequence
+    stranded : str
+        Whether the data to be aligned is from a strand-specific assay.
+        Option is yes, no, reverse.
+        reverse means yes with reversed strand interpretation.
+
+    Returns
+    -------
+    iv_seq :
+        todo update type
+
+    """
     if r[0] is not None and r[0].aligned:
         iv_seq = _get_iv_seq_pe_read_first(com, r, stranded)
     else:
@@ -201,33 +294,59 @@ def _get_iv_seq_pe_read(com, r, stranded):
     return iv_seq
 
 
-def _assess_pe_read(minaqual, multimapped_mode, r, read_stats, secondary_alignment_mode,
+def _assess_pe_read(minaqual, multimapped_mode, read_sequence, read_stats, secondary_alignment_mode,
                     supplementary_alignment_mode):
-    if (r[0] is None) or not (r[0].aligned):
-        read_stats.add_not_aligned_read(read_sequence=r)
+    """
+    Function to check the read for paired end.
+    
+    Parameters
+    ----------
+    minaqual : int
+        Value denoting the MAPQ alignment quality of reads to skip.
+    multimapped_mode : str
+        Whether and how to score reads that are not uniquely aligned or
+        ambiguously assigned to features.
+        Choices: none, all, fraction, random.
+    read_sequence :
+        todo update type
+    read_stats : ReadsStatistics object
+        Object which stores a bunch of statistics about the read sequences.
+    secondary_alignment_mode : str
+        Whether to score secondary alignments (0x100 flag).
+        Choices: score or ignore.
+    supplementary_alignment_mode : str
+        Whether to score supplementary alignments (0x800 flag).
+        Choices: score or ignore.
+
+    Returns
+    -------
+
+    """
+    if (read_sequence[0] is None) or not (read_sequence[0].aligned):
+        read_stats.add_not_aligned_read(read_sequence=read_sequence)
         return True
 
     if secondary_alignment_mode == 'ignore':
-        if (r[0] is not None) and r[0].not_primary_alignment:
+        if (read_sequence[0] is not None) and read_sequence[0].not_primary_alignment:
             return True
-        elif (r[1] is not None) and r[1].not_primary_alignment:
+        elif (read_sequence[1] is not None) and read_sequence[1].not_primary_alignment:
             return True
     if supplementary_alignment_mode == 'ignore':
-        if (r[0] is not None) and r[0].supplementary:
+        if (read_sequence[0] is not None) and read_sequence[0].supplementary:
             return True
-        elif (r[1] is not None) and r[1].supplementary:
+        elif (read_sequence[1] is not None) and read_sequence[1].supplementary:
             return True
     try:
-        if ((r[0] is not None and r[0].optional_field("NH") > 1) or
-                (r[1] is not None and r[1].optional_field("NH") > 1)):
-            read_stats.add_not_unique_read(read_sequence=r)
+        if ((read_sequence[0] is not None and read_sequence[0].optional_field("NH") > 1) or
+                (read_sequence[1] is not None and read_sequence[1].optional_field("NH") > 1)):
+            read_stats.add_not_unique_read(read_sequence=read_sequence)
             if multimapped_mode == 'none':
                 return True
     except KeyError:
         pass
-    if ((r[0] and r[0].aQual < minaqual) or
-            (r[1] and r[1].aQual < minaqual)):
-        read_stats.add_low_quality_read(read_sequence=r)
+    if ((read_sequence[0] and read_sequence[0].aQual < minaqual) or
+            (read_sequence[1] and read_sequence[1].aQual < minaqual)):
+        read_stats.add_low_quality_read(read_sequence=read_sequence)
         return True
     return False
 
